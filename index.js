d40875c2735f3bab18cba0a91ceddb6feaa120fb
Luanny <luanny>
2025-04-22 21:55:29 -0300

    iniciando javascript

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-alert("Hello word");
-const nome ="Luanny";
-let idade= 34;
-let comida favorita
+alert("Hello word!");
 
-console.log(comidaFavorita)
+console.log( "Sou muito utilizado para mostar retornos de parte dos logarítimos,ajudando o programador a debugar");
 
-comidaFavorita = "mungunzá"
+// Exercício: 
+
+//Declarar uma variável com o seu nome 
+//Declarar uma variável com a sua idade
+//Declarar uma variável com a sua comida favorita e não 
+//atribuir valor
+//Atribuir valor a sua comida favorita
+
+const nome = "Luanny";
+
+
+
+let comida favorita = "Pizza";
+
+console.log(comidaFavorita);
+
+comidaFavorita = "Pizza";
+
+console.log(comidaFavorita);
+
+// nome = "Julio
+
+
+nome prompt('Digite seu nome");
+
+console.log(nome);
+
+
+// string 
+
+
+nome = "Nayara"
+let idade = "29"
+console.log(typeof(idade));
+
+//Number
+
+idade = 30
+let altura = 1.53
+
+console.log(typeof(altura));
+
+//Boolean (true or false)
+const desenvolvedoraJavascript = true;  //camelcase
 
 
 
@@ -13,5 +53,5 @@ comidaFavorita = "mungunzá"
 
 
 
-// 
+
 
